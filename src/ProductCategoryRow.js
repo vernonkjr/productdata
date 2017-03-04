b845e7825a928ded84ee466b5ed934f0c0e2ec29
vernonkjr <vkerrjr@gmail.com>
2017-03-04 12:19:19 -0500

    initial version of reactjs project

---
+++ src/ProductCategoryRow.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class ProductCategoryRow extends Component{
+  render(){
+    return (<tr><th colSpan="2">{this.props.category}</th></tr>);
+  }
+}
+
+export default ProductCategoryRow;
